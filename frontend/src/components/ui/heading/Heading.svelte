e44f5a445093c14904c9ebf4324bce07bedbeb9d
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-14 21:11:41 +0200

    HEADING: first commit

---
+++ frontend/src/components/ui/heading/Heading.svelte
@@ -0,0 +1,26 @@
+<div class="fixed top-0 w-full border-b-[1px] border-black">
+	<div class="ml-10 mr-10 flex justify-end items-center">
+		<ul class="my-0 h-16 flex justify-between gap-10 items-center text-fluid-md">
+			<li class="flex items-center">
+				<a href="/">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="2em"
+						height="2em"
+						viewBox="0 0 24 24"
+						class="flex items-center cursor-pointer"
+						><path
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							d="m2 8l9.732-4.866a.6.6 0 0 1 .536 0L22 8m-2 3v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-8"
+						/></svg
+					>
+				</a>
+			</li>
+			<li><a href="/team">team</a></li>
+		</ul>
+	</div>
+</div>
