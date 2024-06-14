4a96f776de8acb1b6258f0ce78c833331682a796
Joschka Eickhoff <jeickhoff@posteo.de>
2024-06-14 21:11:56 +0200

    LAYOUT: first commit

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import Footing from '../components/ui/footing/Footing.svelte';
+	import Heading from '../components/ui/heading/Heading.svelte';
 </script>
 
-<slot />
+<div class="relative min-h-screen flex flex-col items-center justify-center">
+	<Heading />
+	<slot />
+	<Footing />
+</div>
