cca95b8ea7a47d86cd21d21000a3adf02a29ba39
Sebastian Elohim Perrone <elohim2598@gmail.com>
2024-06-14 22:53:13 -0400

    LOGIN: Made some minor refactor

--- frontend/src/routes/auth/login/+page.svelte
+++ frontend/src/routes/auth/login/+page.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
-	let showPassword: boolean = false;
+	// State variables
+	let showPassword: Boolean = false;
+
+	// Buttons array with common styles
+	let buttons = [
+		{
+			className: 'w-full block bg-black text-white border-2 border-black py-3 text-fluid-md mb-4',
+			content: 'Login',
+			buttonType: 'submit' as const
+		},
+		{
+			className: 'w-full block bg-white border-2 border-black text-black py-3 text-fluid-md mb-4',
+			content: 'Login with Google',
+			buttonType: 'button' as const
+		}
+	];
 </script>
 
 <div class="w-full max-w-md space-y-8">
+	<!-- Header -->
 	<div>
 		<h1 class="mt-6 text-center text-5xl font-extrabold">Log In</h1>
 		<h6 class="text-center mt-6 text-fluid-md">Lorem ipsum dolor sit amet adipiscing elit.</h6>
 	</div>
+
+	<!-- Form -->
 	<form action="/login" method="POST" class="mt-8 space-y-6 flex flex-wrap w-full">
+		<!-- Email input -->
 		<div class="w-full px-3">
-			<label class="block uppercase tracking-wide text-gray-700 text-sm font-bold" for="email">
-				*EMAIL
-			</label>
-			<input class=" block w-full bg-white text-gray-700 border py-3 px-3" id="email" type="text" />
+			<label for="email" class="block uppercase tracking-wide text-gray-700 text-sm font-bold"
+				>*EMAIL</label
+			>
+			<input id="email" type="text" class="block w-full bg-white text-gray-700 border py-3 px-3" />
 		</div>
+
+		<!-- Password input -->
 		<div class="w-full px-3">
-			<label class="block uppercase tracking-wide text-gray-700 text-sm font-bold" for="pw">
-				*PASSWORD
-			</label>
+			<label for="pw" class="block uppercase tracking-wide text-gray-700 text-sm font-bold"
+				>*PASSWORD</label
+			>
 			<input
-				class=" block w-full bg-white text-gray-700 border py-3 px-3"
 				id="pw"
 				type={showPassword ? 'text' : 'password'}
+				class="block w-full bg-white text-gray-700 border py-3 px-3"
 			/>
 			<button
 				type="button"
@@ -35,19 +56,14 @@
 				{/if}
 			</button>
 		</div>
+
+		<!-- Buttons -->
 		<div class="w-[95%] mx-auto">
-			<button
-				type="submit"
-				class="w-full block bg-black text-white border-2 border-black py-3 text-fluid-md mb-4"
-			>
-				Login
-			</button>
-			<button
-				class="w-full block bg-white border-2 border-black text-black py-3 text-fluid-md mb-4"
-			>
-				<!-- Logo should go here -->
-				Login with Google
-			</button>
+			{#each buttons as button}
+				<button type={button.buttonType} class={button.className}>
+					{button.content}
+				</button>
+			{/each}
 			<a href="#" class="w-full flex justify-center"><u>Forgot your password?</u></a>
 		</div>
 	</form>
