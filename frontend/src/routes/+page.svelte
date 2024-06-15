40199ce6c0cea4e27488bbdce59e1074ff468d8b
Sebastian Elohim Perrone <elohim2598@gmail.com>
2024-06-14 22:11:46 -0400

    FRONTEND: Added login route

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<script>
-	import '../app.css';
-	import Footing from '../components/ui/footing/Footing.svelte';
-	import Heading from '../components/ui/heading/Heading.svelte';
-</script>
-
 <h1 class="text-center">AI Chatbot Demo</h1>
 <p class="w-3/4 text-fluid-md">
 	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet odit sequi odio tempore sed
 	accusantium minima quae eum! Excepturi aut quos sed at aliquam amet officiis nam magni, maxime
 	molestiae.
 </p>
-<div class="mt-10">
-	<button class="py-3 px-5 border-2 rounded-md">Log In</button>
+<div class="mt-10 text-fluid-md">
+	<a href="/auth/login" class="py-3 px-5 border-2 border-black hover:border-gray-500">Log In</a>
 </div>
---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	let showPassword: boolean = false;
+</script>
+
+<div class="w-full max-w-md space-y-8">
+	<div>
+		<h1 class="mt-6 text-center text-5xl font-extrabold">Log In</h1>
+		<h6 class="text-center mt-6 text-fluid-md">Lorem ipsum dolor sit amet adipiscing elit.</h6>
+	</div>
+	<form action="/login" method="POST" class="mt-8 space-y-6 flex flex-wrap w-full">
+		<div class="w-full px-3">
+			<label class="block uppercase tracking-wide text-gray-700 text-sm font-bold" for="email">
+				*EMAIL
+			</label>
+			<input class=" block w-full bg-white text-gray-700 border py-3 px-3" id="email" type="text" />
+		</div>
+		<div class="w-full px-3">
+			<label class="block uppercase tracking-wide text-gray-700 text-sm font-bold" for="pw">
+				*PASSWORD
+			</label>
+			<input
+				class=" block w-full bg-white text-gray-700 border py-3 px-3"
+				id="pw"
+				type={showPassword ? 'text' : 'password'}
+			/>
+			<button
+				type="button"
+				class="text-fluid-sm text-gray-400 hover:text-gray-900"
+				on:click={() => (showPassword = !showPassword)}
+			>
+				{#if showPassword}
+					Hide Password
+				{:else}
+					Show Password
+				{/if}
+			</button>
+		</div>
+		<div class="w-[95%] mx-auto">
+			<button
+				type="submit"
+				class="w-full block bg-black text-white border-2 border-black py-3 text-fluid-md mb-4"
+			>
+				Login
+			</button>
+			<button
+				class="w-full block bg-white border-2 border-black text-black py-3 text-fluid-md mb-4"
+			>
+				<!-- Logo should go here -->
+				Login with Google
+			</button>
+			<a href="#" class="w-full flex justify-center"><u>Forgot your password?</u></a>
+		</div>
+	</form>
+</div>
