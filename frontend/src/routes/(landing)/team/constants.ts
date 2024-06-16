65678fbc9ad78916b972689643e64156b2fb0ff0
Sebastian Elohim Perrone <elohim2598@gmail.com>
2024-06-16 15:46:42 -0400

    FIX: team images

--- frontend/src/routes/(landing)/team/constants.ts
+++ frontend/src/routes/(landing)/team/constants.ts
@@ -1,18 +1,18 @@
 export const teamMembers = [
     {
-        image: 'https://ibb.co/VDHBwqC',
+        image: 'https://res.cloudinary.com/dn4jd6slb/image/upload/v1718567027/left-stickman_lqipre.jpg',
         name: 'Aden',
         width: 150,
         className: 'scale-x-[-1] mb-3'
     },
     {
-        image: 'https://ibb.co/2c29w0r',
+        image: 'https://res.cloudinary.com/dn4jd6slb/image/upload/v1718567027/middle-stickman_hz9f7y.jpg',
         name: 'Sebastian',
         width: 110,
         className: 'b-3'
     },
     {
-        image: 'https://ibb.co/42ymJ7y',
+        image: 'https://res.cloudinary.com/dn4jd6slb/image/upload/v1718567027/right-stickman_vv7oxv.jpg',
         name: 'Josh',
         width: 150,
         className: 'b-3'
